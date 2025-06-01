aff762309fa7d2bace40edc33920df03609a424b
sapana <sapanadhami1111@gmail.com>
2025-06-01 16:08:31 +0100

    Basic project steup completed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = async  ({ children }: { children: ReactNode }) => {
     <main className="relative flex min-h-screen flex-col text-light-100 sm:flex-row">
       <section className="flex flex-1 items-center justify-center bg-pattern bg-cover bg-top px-5 py-10">
         <div className="gradient-vertical mx-auto flex max-w-xl flex-col gap-6 rounded-lg p-10 bg-dark-100 w-full">
-          <h2 className="text-3xl font-bold text-white text-center">📖 UniLib</h2>
+          <h2 className="text-3xl font-bold text-white text-center">UniLib</h2>
           {children}
         </div>
       </section>
